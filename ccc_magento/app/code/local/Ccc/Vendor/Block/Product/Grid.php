71c7fe20aa52ddc1027f1c80b760eeabe7475362
Anjali8881 <anjalisharma.glsica16@gmail.com>
2021-05-12 10:57:38 +0530

    changes in Block and frontend design added

--- ccc_magento/app/code/local/Ccc/Vendor/Block/Product/Grid.php
+++ ccc_magento/app/code/local/Ccc/Vendor/Block/Product/Grid.php
@@ -15,6 +15,7 @@ class Ccc_Vendor_Block_Product_Grid extends Mage_Core_Block_Template {
 		//$adminStore = Mage_Core_Model_App::ADMIN_STORE_ID;
 
 		$collection->joinAttribute('vendor_status', 'vendor_product/vendor_status', 'entity_id', null, 'inner');
+		$collection->joinAttribute('admin_status', 'vendor_product/admin_status', 'entity_id', null, 'left');
 		$collection->joinAttribute('price', 'vendor_product/price', 'entity_id', null, 'inner');
 		$collection->joinAttribute('name', 'vendor_product/name', 'entity_id', null, 'inner');
 		//$collection->joinAttribute('admin_status', 'vendor_product/admin_status', 'entity_id', null, 'inner');
