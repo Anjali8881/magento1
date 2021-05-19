234f883599cd23d8ac59c23a46a30bba63f5f789
Anjali8881 <anjalisharma.glsica16@gmail.com>
2021-05-19 20:10:23 +0530

    Changes in Attribute Edit

--- ccc_magento/app/code/local/Ccc/Vendor/Block/Attribute/Edit.php
+++ ccc_magento/app/code/local/Ccc/Vendor/Block/Attribute/Edit.php
@@ -76,5 +76,103 @@ class Ccc_Vendor_Block_Attribute_Edit extends Mage_Core_Block_Template {
 		return $groupId;
 	}
 
+	public function getOptionValues() {
+		$attributeType = $this->getAttribute()->getFrontendInput();
+		$defaultValues = $this->getAttribute()->getDefaultValue();
+		if ($attributeType == 'select' || $attributeType == 'multiselect') {
+			$defaultValues = explode(',', $defaultValues);
+		} else {
+			$defaultValues = array();
+		}
+
+		switch ($attributeType) {
+		case 'select':
+			$inputType = 'radio';
+			break;
+		case 'multiselect':
+			$inputType = 'checkbox';
+			break;
+		default:
+			$inputType = '';
+			break;
+		}
+
+		$values = $this->getData('option_values');
+		if (is_null($values)) {
+			$values = array();
+			$optionCollection = Mage::getResourceModel('eav/entity_attribute_option_collection')
+				->setAttributeFilter($this->getAttribute()->getId())
+				->setPositionOrder('desc', true)
+				->load();
+
+			$helper = Mage::helper('core');
+			foreach ($optionCollection as $option) {
+				$value = array();
+				if (in_array($option->getId(), $defaultValues)) {
+					$value['checked'] = 'checked="checked"';
+				} else {
+					$value['checked'] = '';
+				}
+
+				$value['intype'] = $inputType;
+				$value['id'] = $option->getId();
+				$value['sort_order'] = $option->getSortOrder();
+				foreach ($this->getStores() as $store) {
+					$storeValues = $this->getStoreOptionValues($store->getId());
+					$value['store' . $store->getId()] = isset($storeValues[$option->getId()])
+					? $helper->escapeHtml($storeValues[$option->getId()]) : '';
+				}
+				$values[] = new Varien_Object($value);
+			}
+			$this->setData('option_values', $values);
+		}
+
+		return $values;
+	}
+
+	public function getStoreOptionValues($storeId) {
+		$values = $this->getData('store_option_values_' . $storeId);
+		if (is_null($values)) {
+			$values = array();
+			$valuesCollection = Mage::getResourceModel('eav/entity_attribute_option_collection')
+				->setAttributeFilter($this->getAttribute()->getId())
+				->setStoreFilter($storeId, false)
+				->load();
+			foreach ($valuesCollection as $item) {
+				$values[$item->getId()] = $item->getValue();
+			}
+			$this->setData('store_option_values_' . $storeId, $values);
+		}
+		return $values;
+	}
+
+	public function getLabelValues() {
+		$values = array();
+		$frontendLabel = $this->getAttribute()->getFrontend()->getLabel();
+		if (is_array($frontendLabel)) {
+			return $frontendLabel;
+		}
+		$values[0] = $frontendLabel;
+		$storeLabels = $this->getAttribute()->getStoreLabels();
+		foreach ($this->getStores() as $store) {
+			if ($store->getId() != 0) {
+				$values[$store->getId()] = isset($storeLabels[$store->getId()]) ? $storeLabels[$store->getId()] : '';
+			}
+		}
+		return $values;
+	}
+
+	public function getStores() {
+		$stores = $this->getData('stores');
+		if (is_null($stores)) {
+			$stores = Mage::getModel('core/store')
+				->getResourceCollection()
+				->setLoadDefault(true)
+				->load();
+			$this->setData('stores', $stores);
+		}
+		return $stores;
+	}
+
 }
 ?>
