f53e84eff2c02e9418af93a55247f85da4eac65e
Anjali8881 <anjalisharma.glsica16@gmail.com>
2021-05-20 19:26:40 +0530

    Payment method

---
+++ ccc_magento/app/code/local/Ccc/MyPaymentMethod/Model/Standard.php
@@ -0,0 +1,12 @@
+<?php
+class Ccc_MyPaymentMethod_Model_Standard extends Mage_Payment_Model_Method_Abstract {
+	protected $_code = 'mypaymentmethod';
+	// protected $_isInitializeNeeded = true;
+	// protected $_canUseInternal = false;
+	// protected $_canUseForMultishipping = false;
+
+	// public function getOrderPlaceRedirectUrl() {
+	// 	return Mage::getUrl('customcard/standard/redirect', array('_secure' => true));
+	// }
+}
+?>
