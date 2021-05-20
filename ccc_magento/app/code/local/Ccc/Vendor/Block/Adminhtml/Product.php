d061b381756cd34f118c3b7c8f6ba2957cf2af48
Anjali8881 <anjalisharma.glsica16@gmail.com>
2021-05-20 12:03:48 +0530

    Add Button removed

--- ccc_magento/app/code/local/Ccc/Vendor/Block/Adminhtml/Product.php
+++ ccc_magento/app/code/local/Ccc/Vendor/Block/Adminhtml/Product.php
@@ -2,10 +2,11 @@
 class Ccc_Vendor_Block_Adminhtml_Product extends Mage_Adminhtml_Block_Widget_Grid_Container {
 
 	function __construct() {
+		parent::__construct();
 		$this->_controller = "adminhtml_product";
 		$this->_blockGroup = 'vendor';
 		$this->_headerText = 'Manage Vendor Product';
-		parent::__construct();
+		$this->_removeButton('add');
 	}
 }
 
