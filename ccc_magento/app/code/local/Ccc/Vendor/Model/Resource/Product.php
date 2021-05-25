4a6b0224cb14c2d32351a1923f65c1227f9f287f
Anjali8881 <anjalisharma.glsica16@gmail.com>
2021-05-25 19:30:13 +0530

    price sku name validation

--- ccc_magento/app/code/local/Ccc/Vendor/Model/Resource/Product.php
+++ ccc_magento/app/code/local/Ccc/Vendor/Model/Resource/Product.php
@@ -15,5 +15,24 @@ class Ccc_Vendor_Model_Resource_Product extends Mage_Catalog_Model_Resource_Prod
 	protected function _getDefaultAttributes() {
 		return array('entity_id', 'entity_type_id', 'attribute_set_id', 'type_id', 'created_at', 'updated_at');
 	}
+
+	public function getSkuById($sku) {
+		$adapter = $this->_getReadAdapter();
+
+		$select = $adapter->select()
+			->from($this->getEntityTable(), 'entity_id')
+			->where('sku = :sku');
+
+		$bind = array(':sku' => (string) $sku);
+
+		return $adapter->fetchOne($select, $bind);
+	}
+
+	protected function _getReadAdapter() {
+		if (is_string($this->_read)) {
+			$this->_read = Mage::getSingleton('core/resource')->getConnection($this->_read);
+		}
+		return $this->_read;
+	}
 }
 ?>
